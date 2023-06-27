c0260a5f072a383ea0eddea3b8fe16eb2ce49e92
Kiberpraim <abdykulovalmanbet@gmail.com>
2023-06-27 16:31:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson35/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lesson35;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Собрать из букв имён своё имя*/
+
+
+        String[] names = {"Чынгыз", "Алексей", "Андрей", "Алинур", "Максим", "Бектур", "Тимур", "Настя", "Алим"};
+
+        String me = "Алманбет";
+        String mee = "АЛМАНБЕТ";
+
+        String result="";
+
+        boolean f = false;
+
+
+        for (int i = 0; i < me.length(); i++) {
+            for (String name : names) {
+                f = false;
+                for (int j = 0; j < name.length(); j++) {
+                    if (name.charAt(j) == me.charAt(i) || name.charAt(j) == mee.charAt(i) ) {
+                        result = result + name.charAt(j);
+                        f = true;
+                        break;
+                    }
+                }
+                if (f) {
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+}
